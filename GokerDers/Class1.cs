d431ca6eb5724f195962b093784ecadc6acfa898
Goker <goker.cetin@index.com.tr>
2023-05-24 22:34:58 +0300

    FirstCommitHayirliOlsun

---
+++ GokerDers/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Dersler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max(int[] array)
+
+            {
+                int toplama = 0;
+
+                foreach (int toplam in array)
+                {
+                    toplama += toplam;
+                    Console.WriteLine(toplam);
+
+                }
+                return toplama;
+
+
+            }
+
+            int toplama = max(new int[3] { 1, 2, 3 });
+
+            Console.WriteLine(toplama);
+
+
+            int ortalama(int[] array)
+            {
+                int item = 0;
+
+                foreach (int ortala in array)
+                {
+                    item = item + ortala;
+                    Console.WriteLine(item);
+
+                }
+                return item / array.Length;
+            }
+            int item = ortalama(new int[5] { 1, 2, 3, 4, 5 });
+
+            Console.WriteLine(item);
+
+            int maximum(int[] dizi)
+            {
+                int max = 0;
+                int[] array = dizi;
+
+                foreach (int maximum in dizi)
+                {
+                    foreach (int maximum2 in array)
+                    {
+                        if (maximum > maximum2 && max < maximum)
+                        {
+                            max = maximum;
+
+                        }
+                    }
+
+
+                }
+                return max;
+            }
+            Console.WriteLine(maximum(new int[5] { 1, 2, 7, 9, 5 }));
+
+
+        }
+    }
+
+
+
+
+
+
+}
---
+++ GokerDers/Class2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dersler.OOP
+{
+    public class Malzeme
+    {
+        public string Un { get; set; }
+
+        public string Şeker { get; set; }
+
+        public string Su { get; set; }
+
+    }
+    public class Ekmek : Malzeme, IPisenler
+    {
+        public int Durum()
+        {
+            return 0;
+        }
+
+        public string Name()
+        {
+            return "ekmek";
+        }
+    }
+
+    public interface IPisenler
+    {
+        string Name();
+        int Durum();
+    }
+    public class Poğaca : Malzeme, IPisenler
+    {
+        public int _üretimAdedi = 10;
+        public int Durum()
+        {
+            return 1;
+        }
+        public string Name()
+        {
+            return "poğaca";
+        }
+    }
+
+
+
+
+
+}
