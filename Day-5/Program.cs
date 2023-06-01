d771477e3c77bdf0d8286bd60e713bac968e96d7
guneyunus <guney.yunus.dev@outlook.com>
2023-06-01 23:29:19 +0300

    .

--- Day-5/Program.cs
+++ Day-5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 public class Program
 {
@@ -26,19 +27,63 @@ public class Program
         ////denemeObjecsi.setYasIkı();
         ///
 
-        HesapMakinesi hesapmakinasi = new HesapMakinesi();
+        //HesapMakinesi hesapmakinasi = new HesapMakinesi();
         //hesapmakinasi.Cikar(5,3);
 
         //CikarmaHandler cikarmaHandler = new CikarmaHandler(hesapmakinasi.Cikar);
 
-        
+
         //Console.WriteLine(cikarmaHandler(2, 1));
 
         //Console.WriteLine(cikarmaHandler.Invoke(5, 3));
 
+        //public static int CarpmaIslemi(int a)
+        //{
+        //    return a * (a - 1);
+        //}
+
+        //Fuc<T,TRESULT>
         Func<int, int> CarpFunc = HesapMakinesi.CarpmaIslemi;
         int b = CarpFunc(5);
         Console.WriteLine(b);
+
+
+        int bankCode = 1000;
+
+        Func<bool> bankaVarMı = () =>
+        {
+            if (bankCode == 1000)
+            {
+                return true;
+            }
+            else if(bankCode == 1100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        };
+
+        bool varlikDurumu = bankaVarMı();
+
+        if (bankaVarMı())
+        {
+            Console.WriteLine();
+        }
+
+        List<int> sayilar = new List<int> { 1, 2, 3,4,5,6,7,8,9,10 };
+
+        List<int> IkiyeBolunenler = sayilar.Where(q => q % 2 == 0 ).Where(x => x - 1 > 0).ToList();
+        List<int> IkiyeBolunenler2 = sayilar.Where(q => q % 2 == 0 && q - 1 > 0 ).ToList();
+
+        List<string> Alfabe = new List<string>() { "A","B","C","D","E","F"};
+
+        List<string> Sesliler = Alfabe.Where(q => q.Any()).ToList();
+
+        var sesliler = Alfabe.Where(q => q.Contains('e')).ToArray();
 
 
 
