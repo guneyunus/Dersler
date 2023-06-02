621bb4749b44c0bfe4f2bce5fb4a0b093f2fb3c4
Goker <goker.cetin@index.com.tr>
2023-06-02 21:49:45 +0300

    Day6 Çalışma Göker

---
+++ Day-6/Entities/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6.Entities
+{
+    public abstract class Animal
+    {
+        public bool Cinsiyet { get; set; }
+
+        public DateTime Yas { get; set; }
+
+        public string Beslen() 
+        { 
+            return "";
+        }
+
+        public interface ISudaYuzen
+        {
+            string Yuz(); // metot bodysi olmamalı. Sadece imza yeterli
+            
+        }
+        public interface IKaradaGiden
+        {
+            string Yuru();
+
+        }
+
+        public class kopekbalıgı : Animal, ISudaYuzen
+        {
+            public string Yuz()
+            {
+                return "yüzüyorum";
+            }
+        }
+        public class Aslan : Animal, IKaradaGiden
+        {
+            public string Yuru()
+            {
+                return "yürüyorum";
+            }
+            public override string ToString()
+            {
+                return "deneme";
+            }
+        }
+        public class Timsah : Animal, ISudaYuzen, IKaradaGiden
+        {
+            public string Yuru()
+            {
+                return "Timsah gözyaşı döküyorum";
+            }
+
+            public string Yuz()
+            {
+                return "Timsah yüzüşü yapıyorum";
+            }
+        }
+    }
+}
