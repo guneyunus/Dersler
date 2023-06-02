f631a6cc91f3fa2245ee36614d7e5785c3588825
guneyunus <guney.yunus.dev@outlook.com>
2023-06-02 22:26:47 +0300

    .

--- Question-2/Program.cs
+++ Question-2/Program.cs
@@ -1,28 +1,15 @@
 ﻿//Örnek 2: Girilen bir sayının asal olduğunu bulan ve bu sayıdan sonraki 5 asal sayıyı ekrana yazan program:
 
-int input = Convert.ToInt32(Console.ReadLine());
-
-int asal = input;
-int counter = 0;
-
-for (int i = 2; i < input; i++)
+public class Program
 {
-    if (asal % i == 0)
-    { counter++; }
-}
 
-if (counter != 0)
-{
-    Console.WriteLine("Sayı Asal Değildir.");
-}
-else
-{
-    Console.WriteLine("Sayı Asaldır.");
-    int sayac = 0;
-    do
-    {       
-        Console.WriteLine(input);
-        input++;
+    public static void Main()
+    {
+
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        int asal = input;
+        int counter = 0;
 
         for (int i = 2; i < input; i++)
         {
@@ -30,16 +17,36 @@ else
             { counter++; }
         }
 
-        if (counter == 0)
+        if (counter != 0)
         {
-            sayac++;
-            Console.WriteLine(input);
+            Console.WriteLine("Sayı Asal Değildir.");
         }
+        else
+        {
+            Console.WriteLine("Sayı Asaldır.");
+            int sayac = 0;
+            do
+            {
+                Console.WriteLine(input);
+                input++;
 
-    } while(sayac==5);
+                for (int i = 2; i < input; i++)
+                {
+                    if (asal % i == 0)
+                    { counter++; }
+                }
 
-   
+                if (counter == 0)
+                {
+                    sayac++;
+                    Console.WriteLine(input);
+                }
 
+            } while (sayac == 5);
+
+
+
+        }
+        Console.ReadKey();
+    }
 }
-Console.ReadKey();
-
