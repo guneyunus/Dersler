4c112a1a6e6de9873c36eab10d297a2685347908
Semih Karaoğlan <semihkaraoglan@icloud.com>
2023-06-07 16:49:57 +0300

    20

---
+++ Question-20/Program.cs
@@ -0,0 +1,9 @@
+﻿//        Örnek 20: İçerisinde isimler bulunan string bir diziyi en son elemandan itibaren ekrana yazdıran örnek:
+
+string[] names = new string[] { "semih", "göker", "deniz", "mustafa", "burak","yunus" };
+
+
+for (int i = names.Length-1; i >= 0 ; i--)
+{
+    Console.WriteLine($"{i+1}. eleman: "+names[i]);
+}
